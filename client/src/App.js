2b77ccf062abafac5dbd6d46fe8b6e0bd1dddc53
gold_chen_five <rachel7465022@gmail.com>
2023-04-13 10:16:39 +0800

    hashrouter add

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Routes, Route,HashRouter  } from "react-router-dom";
 import HomePage from "scenes/homepage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline/>
           <Routes>
@@ -24,7 +24,7 @@ function App() {
             <Route path="/profile/:userId" element={isAuth ? <ProfilePage/> : <Navigate to='/'/>}/>
           </Routes>
         </ThemeProvider>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
